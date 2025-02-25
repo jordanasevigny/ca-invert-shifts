0982a695c695f773c6be7c8c77cdf0a673466444
jordanasevigny <jsevigny@ucsc.edu>
2025-02-25 08:59:55 -0800

    updated code & data files to include recent calcofi data

--- scripts/preliminary_map_range_edge_shifts_by_species.R
+++ scripts/preliminary_map_range_edge_shifts_by_species.R
@@ -22,18 +22,18 @@ class(world)
 
 # load in data
 # poor naming scheme but the file with the similar name but excel has two sheets with the other having the original excerpt
-raw <- read.csv("Trial_CalCOFI_Report_Data_R_format_8-2024.csv")
+raw <- read.csv("processed_data/Trial_CalCOFI_Report_Data_R_format_2-2025.csv")
 
 # drop unused columns, select distinct rows only (when a single species has multiple recorded shifts, their origin is repeated, we don't need that)
 df <- data.frame(raw) %>%
-  select(-c(Report_Volume, Observation_Date, ENSO, Notes)) %>%
+  select(-c(Report_Volume, Observation_Date, Notes)) %>%
   distinct()
 
 # need to add some longitude jitter into the points so they all show up
 # Initialize a counter for each word
 loc_count <- list()
 # longitude error base amount
-er <- 1.25
+er <- -1.25
 
 # Loop through each row of the data frame
 for (i in 1:nrow(df)) {
@@ -60,4 +60,18 @@ ggplot(data = world) +
   coord_sf(xlim = c(-110, -150), ylim = c(30, 63), expand = FALSE)
 
 
+# map with ENSO/species points
+ggplot(data = world) +
+  geom_sf() +
+  geom_point(data = df, aes(x = Longitude, y = Latitude, shape = ENSO, color = Species), size = 2.5) +
+  xlab("Longitude") + ylab("Latitude") +
+  labs(color="Range Edge") +
+  coord_sf(xlim = c(-110, -150), ylim = c(30, 63), expand = FALSE)
 
+ggplot(data = world) +
+  geom_sf() +
+  geom_point(data = df, aes(x = Longitude, y = Latitude, shape = ENSO, color = Old_or_New), size = 2.5) +
+  xlab("Longitude") + ylab("Latitude") +
+  labs(color = "Range Edge", shape = "ENSO Phase") +
+  coord_sf(xlim = c(-150, -110), ylim = c(30, 63), expand = FALSE) +
+  facet_wrap(~Species)  # Facet by species
