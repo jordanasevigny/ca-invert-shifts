c04bb2ab45258a12e8567bfd5c868f65b0cc9a2e
jordanasevigny <jsevigny@ucsc.edu>
2025-10-08 11:07:59 -0700

    extension table

---
+++ scripts/summary_extension_table.R
@@ -0,0 +1,69 @@
+# Extension supplementary table
+# By: Jordana Sevigny, jordana.sevigny@gmail.com
+# Date created: 10/08/2025
+
+rm(list = ls())
+
+# Load libraries
+library("ggplot2")
+theme_set(theme_bw())
+library(dplyr)
+library(geosphere)
+library(gt)
+
+
+# Load review data
+df <- read.csv("processed_data/merged_calcofi_lab_review.csv")
+
+# Calculate extension distance
+# Distance function
+get_distance_km <- function(lat1, lon1, lat2, lon2) {
+  point1 <- c(lon1, lat1)
+  point2 <- c(lon2, lat2)
+  dist_meters <- distHaversine(point1, point2)
+  return(dist_meters / 1000)  # convert to kilometers
+}
+
+# Apply function row-wise
+ext_distance <- df %>%
+  rowwise() %>%
+  mutate(distance_km = get_distance_km(hist_range_lat, hist_range_lon, latitude, longitude)) %>%
+  ungroup()
+
+# Average/sd/max/mix extension distance 1) by max per event 2) by all observations
+extensions <- ext_distance %>%
+  group_by(latin_name, group_id, first_year) %>%
+  summarise(max_dist = max(distance_km, na.rm = TRUE)) 
+
+
+summary_table <- extensions %>%
+  group_by(latin_name) %>%
+  summarise(
+    mean_extension = mean(max_dist, na.rm = TRUE),
+    extension_years = paste(sort(unique(first_year)), collapse = ", ")
+  ) %>%
+  arrange(latin_name) %>%
+  # optional: round the mean
+  mutate(mean_extension = round(mean_extension, 1))
+
+# Make it look nice for publication
+gt_table <- summary_table %>%
+  gt() %>%
+  fmt_number(columns = mean_extension, decimals = 1) %>%
+  cols_label(
+    latin_name = "Species",
+    mean_extension = "Mean Extension Distance (km)",
+    extension_years = "Years Observed"
+  ) %>%
+  tab_header(
+    title = "Summary of Range Extension Events",
+    subtitle = "Mean northward extension distance and extension year observed per species"
+  ) %>%
+  tab_options(
+    table.font.names = "Helvetica",
+    data_row.padding = px(4),
+    table.font.size = 12,
+    heading.align = "left"
+  )
+
+gtsave(gt_table, "figures/extension_table.png")
